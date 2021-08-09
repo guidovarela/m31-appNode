78cabaa7c2c399e308518138f0adbe771734de56
Guido Varela Coletta <guido.varela@gmail.com>
2021-08-09 15:32:38 -0300

    update db conection

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 //Conexion MySQL
 const Sequelize = require('sequelize')
 
-//conexion local -> wamp
+/*conexion local -> wamp
 let database = 'utn2021'
 let userMYSQL = 'root'
 let passMySQL = ''
-let hostMySQL = '127.0.0.1'
+let hostMySQL = '127.0.0.1'*/
 
-
+//Conexion a Heroku
+//mysql://b9d288e326c067:ad7d4dbf@eu-cdbr-west-01.cleardb.com/heroku_7ec77051f3a3e63?reconnect=true
+let database = "heroku_7ec77051f3a3e63"
+let userMYSQL = "b9d288e326c067"
+let passMySQL = "ad7d4dbf"
+let hostMySQL = "eu-cdbr-west-01.cleardb.com"
 
 const db = new Sequelize(database, userMYSQL, passMySQL, {
   host: hostMySQL,
